14ecc0b57bcf71f8d8e29ca4db68006b76e1c21e
Fernando Gamarra <fgamarra68@gmail.com>
2022-04-11 23:54:23 +0200

    Initial Commit

---
+++ AWalkInTheMarvel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AWalkInTheMarvel
+//
+//  Created by Fernando Gamarra on 11/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
